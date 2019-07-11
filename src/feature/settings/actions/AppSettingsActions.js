e054fd3113e927ba2243c287b227d95ef20172ef
Mikko Vestola <mikko.vestola@iki.fi>
2019-07-11 17:28:35 +0300

    Ignore settings saved if same value

--- src/feature/settings/actions/AppSettingsActions.js
+++ src/feature/settings/actions/AppSettingsActions.js
@@ -6,26 +6,30 @@ import Analytics from "../../common/util/Analytics"
 class AppSettingsActions {
     @action
     saveLanguage(language) {
-        AppSettingsDomainStore.language = language
-        AppSettingsDomainStore.persistSettingsData()
-        Analytics.logEvent("Language setting changed")
+        if (language !== AppSettingsDomainStore.language) {
+            AppSettingsDomainStore.language = language
+            AppSettingsDomainStore.persistSettingsData()
+            Analytics.logEvent("Language setting changed")
+        }
     }
 
     @action
     saveShowAds(showAds) {
-        AppSettingsDomainStore.showAds = showAds
-        if (!showAds) {
-            Alert.alert(
-                "Vahvista muutos",
-                "Mainoksia näyttämällä tuet ohjelman kehitystä. Haluatko varmasti poistaa mainokset?",
-                [
-                    { text: "Peruuta", style: "cancel", onPress: () => this._doSaveShowAds(true) },
-                    { text: "OK", onPress: () => this._doSaveShowAds(false) },
-                ],
-                { cancelable: false },
-            )
-        } else {
-            this._doSaveShowAds(showAds)
+        if (showAds !== AppSettingsDomainStore.showAds) {
+            AppSettingsDomainStore.showAds = showAds
+            if (!showAds) {
+                Alert.alert(
+                    "Vahvista muutos",
+                    "Mainoksia näyttämällä tuet ohjelman kehitystä. Haluatko varmasti poistaa mainokset?",
+                    [
+                        { text: "Peruuta", style: "cancel", onPress: () => this._doSaveShowAds(true) },
+                        { text: "OK", onPress: () => this._doSaveShowAds(false) },
+                    ],
+                    { cancelable: false },
+                )
+            } else {
+                this._doSaveShowAds(showAds)
+            }
         }
     }
 
