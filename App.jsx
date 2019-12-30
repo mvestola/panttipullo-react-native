9c7e122bc070b86d56817ebe12455877b83bb316
Mikko Vestola <mikko.vestola@iki.fi>
2019-12-30 17:39:49 +0200

    Include App.jsx to eslint also and fix formatting.

--- App.jsx
+++ App.jsx
@@ -1,22 +1,22 @@
 import React from "react"
-import {observer} from "mobx-react"
-import {View} from "native-base"
+import { observer } from "mobx-react"
+import { View } from "native-base"
 import { AppContentView } from "./src/feature/app/view/AppContentView"
-import {AppHeaderView} from "./src/feature/app/view/AppHeaderView"
-import {AppUiState} from "./src/feature/app/state/AppUiState"
-import {AppInitializingView} from "./src/feature/app/view/AppInitializingView"
-import {SettingsDomainStore} from "./src/feature/settings/state/SettingsDomainStore"
+import { AppHeaderView } from "./src/feature/app/view/AppHeaderView"
+import { AppUiState } from "./src/feature/app/state/AppUiState"
+import { AppInitializingView } from "./src/feature/app/view/AppInitializingView"
+import { SettingsDomainStore } from "./src/feature/settings/state/SettingsDomainStore"
 
 const App = observer(() => {
-    if (AppUiState.showAppIsInitialising) {
-        return <AppInitializingView/>
-    }
-    return (
-        <View style={{flex: 1}}>
-            <AppHeaderView/>
-            <AppContentView key={`language-${SettingsDomainStore.language}`} />
-        </View>
-    )
+  if (AppUiState.showAppIsInitialising) {
+    return <AppInitializingView />
+  }
+  return (
+    <View style={{ flex: 1 }}>
+      <AppHeaderView />
+      <AppContentView key={`language-${SettingsDomainStore.language}`} />
+    </View>
+  )
 })
 
 export default App
