566bc3ed47f6ec1e6cde8999adab6790f0869720
Mikko Vestola <mikko.vestola@iki.fi>
2019-12-30 16:06:25 +0200

    Fix landscape layout in help and info

--- src/feature/support/HelpView.jsx
+++ src/feature/support/HelpView.jsx
@@ -3,19 +3,22 @@ import React from "react"
 import {
   Body, Card, CardItem, Icon, Text, View,
 } from "native-base"
+import { ScrollView } from "react-native"
 
 export const HelpView = () => (
-  <Card style={{ marginBottom: 20 }}>
-    <CardItem header>
-      <View style={{ flex: 1, flexDirection: "row", alignItems: "center" }}>
-        <Icon name="help-circle" type="MaterialCommunityIcons" style={{ fontSize: 26, color: "blue" }} />
-        <Text>{` ${i18n.t("appInstructionsTitle")}`}</Text>
-      </View>
-    </CardItem>
-    <CardItem>
-      <Body>
-        <Text>{i18n.t("appInstructions")}</Text>
-      </Body>
-    </CardItem>
-  </Card>
+  <ScrollView>
+    <Card style={{ marginBottom: 20 }}>
+      <CardItem header>
+        <View style={{ flex: 1, flexDirection: "row", alignItems: "center" }}>
+          <Icon name="help-circle" type="MaterialCommunityIcons" style={{ fontSize: 26, color: "blue" }} />
+          <Text>{` ${i18n.t("appInstructionsTitle")}`}</Text>
+        </View>
+      </CardItem>
+      <CardItem>
+        <Body>
+          <Text>{i18n.t("appInstructions")}</Text>
+        </Body>
+      </CardItem>
+    </Card>
+  </ScrollView>
 )
--- src/feature/support/InfoView.jsx
+++ src/feature/support/InfoView.jsx
@@ -5,48 +5,50 @@ import {
 } from "native-base"
 import { observer } from "mobx-react"
 import Hyperlink from "react-native-hyperlink"
-import { StyleSheet } from "react-native"
+import { ScrollView, StyleSheet } from "react-native"
 import { SettingsDomainStore } from "../settings/state/SettingsDomainStore"
 
 export const InfoView = observer(() => (
-  <Card>
-    <CardItem header>
-      <Text>{`Panttipullo v${SettingsDomainStore.getAppVersion()}`}</Text>
-    </CardItem>
-    <CardItem>
-      <Body>
-        <Hyperlink linkDefault linkStyle={styles.hyperlink}>
-          <Text>{i18n.t("appDescription")}</Text>
-        </Hyperlink>
-      </Body>
-    </CardItem>
-    <CardItem>
-      <Body>
-        <Hyperlink
-          linkDefault
-          linkStyle={[styles.hyperlink, styles.appLink]}
-          linkText={i18n.t("appHomepage")}
-        >
-          <Text style={styles.appLink}>
-                    https://mvestola.github.io/panttipullo-react-native/
-          </Text>
-        </Hyperlink>
-      </Body>
-    </CardItem>
-    <CardItem>
-      <Body>
-        <Hyperlink
-          linkDefault
-          linkStyle={[styles.hyperlink, styles.appLink]}
-          linkText={i18n.t("privacyPolicy")}
-        >
-          <Text style={styles.appLink}>
-                    https://mvestola.github.io/panttipullo-react-native/privacy-policy.html
-          </Text>
-        </Hyperlink>
-      </Body>
-    </CardItem>
-  </Card>
+  <ScrollView>
+    <Card>
+      <CardItem header>
+        <Text>{`Panttipullo v${SettingsDomainStore.getAppVersion()}`}</Text>
+      </CardItem>
+      <CardItem>
+        <Body>
+          <Hyperlink linkDefault linkStyle={styles.hyperlink}>
+            <Text>{i18n.t("appDescription")}</Text>
+          </Hyperlink>
+        </Body>
+      </CardItem>
+      <CardItem>
+        <Body>
+          <Hyperlink
+            linkDefault
+            linkStyle={[styles.hyperlink, styles.appLink]}
+            linkText={i18n.t("appHomepage")}
+          >
+            <Text style={styles.appLink}>
+                        https://mvestola.github.io/panttipullo-react-native/
+            </Text>
+          </Hyperlink>
+        </Body>
+      </CardItem>
+      <CardItem>
+        <Body>
+          <Hyperlink
+            linkDefault
+            linkStyle={[styles.hyperlink, styles.appLink]}
+            linkText={i18n.t("privacyPolicy")}
+          >
+            <Text style={styles.appLink}>
+                        https://mvestola.github.io/panttipullo-react-native/privacy-policy.html
+            </Text>
+          </Hyperlink>
+        </Body>
+      </CardItem>
+    </Card>
+  </ScrollView>
 ))
 
 const styles = StyleSheet.create({
